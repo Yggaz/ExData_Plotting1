9731c45bb7725b78f21d259c9f07b56eca5787a5
Evgeny Petrov <petrove@prognoz.ru>
2015-01-11 23:44:27 +0500

    Fixed some typos

--- plot4.R
+++ plot4.R
@@ -17,10 +17,10 @@ png("plot4.png", width = 480, height = 480)
 par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
 
 #Chart 1
-plot(data1$DateTime,data1$Global_active_power,ylab="Global Active Power (kilowatts)",xlab="",lwd=1,type="l")
+plot(data1$DateTime,data1$Global_active_power,ylab="Global Active Power",xlab="",lwd=1,type="l")
 
 #Chart2
-plot(data1$DateTime,data1$Voltage,ylab="Voltage",xlab="DateTime",lwd=1,type="l")
+plot(data1$DateTime,data1$Voltage,ylab="Voltage",xlab="datetime",lwd=1,type="l")
 
 #Chart3
 plot(data1$DateTime,data1$Sub_metering_1,ylab="Energy sub metering",xlab="",lwd=1,type="l")
@@ -29,6 +29,6 @@ lines(data1$DateTime,data1$Sub_metering_3,col="blue")
 legend("topright",c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty=1, col=c("black", "red", "blue"))
 
 #Chart 4
-plot(data1$DateTime,data1$Global_reactive_power,ylab="Global_rective_power",xlab="DateTime",lwd=1,type="l")
+plot(data1$DateTime,data1$Global_reactive_power,ylab="Global_reactive_power",xlab="datetime",lwd=1,type="l")
 
 dev.off()
